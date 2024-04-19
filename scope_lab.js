d538a8635655ab1d2fa7612121b882ac7809870f
Vladkovriz <xiaomomi3node@gmail.com>
2024-04-19 13:30:04 -0400

    third Lesson

--- scope_lab.js
+++ scope_lab.js
@@ -41,58 +41,56 @@
 
 //! Second Lesson
 
-let userRole = "admin";
-let accessLevel;
+// let userRole = "admin";
+// let accessLevel;
 
-if (userRole === "admin") {
-    accessLevel = "Full access granted";
-} else if (userRole === "manager") {
-    accessLevel = "Limited access granted";
-} else {
-    accessLevel = "No access granted";
-}
-console.log("Access Level:", accessLevel);
+// if (userRole === "admin") {
+//     accessLevel = "Full access granted";
+// } else if (userRole === "manager") {
+//     accessLevel = "Limited access granted";
+// } else {
+//     accessLevel = "No access granted";
+// }
+// console.log("Access Level:", accessLevel);
 
-let isLoggedIn = true;
-let userMessage;
+// let isLoggedIn = true;
+// let userMessage;
 
-if (isLoggedIn) {
-    if (userRole === "admin") {
-        userMessage = "Welcome, Admin!";
-    } else {
-        userMessage = "Welcome, User!";
-    }
-} else {
-    userMessage = "Please log in to access the system.";
-}
-console.log("User Message:", userMessage);
+// if (isLoggedIn) {
+//     if (userRole === "admin") {
+//         userMessage = "Welcome, Admin!";
+//     } else {
+//         userMessage = "Welcome, User!";
+//     }
+// } else {
+//     userMessage = "Please log in to access the system.";
+// }
+// console.log("User Message:", userMessage);
 
-let userType = "subscriber";
-let userCategory;
+// let userType = "subscriber";
+// let userCategory;
 
-switch (userType) {
-    case "admin":
-        userCategory = "Administrator";
-        break;
-    case "manager":
-        userCategory = "Manager";
-        break;
-    case "subscriber":
-        userCategory = "Subscriber";
-        break;
-    default:
-        userCategory = "Unknown";
-}
+// switch (userType) {
+//     case "admin":
+//         userCategory = "Administrator";
+//         break;
+//     case "manager":
+//         userCategory = "Manager";
+//         break;
+//     case "subscriber":
+//         userCategory = "Subscriber";
+//         break;
+//     default:
+//         userCategory = "Unknown";
+// }
 
-console.log("User Category:", userCategory);
+// console.log("User Category:", userCategory);
 
-let isAuthenticated = true;
+// let isAuthenticated = true;
 
-let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+// let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
-console.log("Authentication Status:", authenticationStatus);
-
-
+// console.log("Authentication Status:", authenticationStatus);
 
 
 
